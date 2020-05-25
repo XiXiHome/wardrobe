7b9a77c02cfe32eacc0a23e5133dfd9e38ef4acc
DigoWu <65758028+DigoWu@users.noreply.github.com>
2020-05-25 11:39:08 +0800

    Create jsonDetails.js

---
+++ js/jsonDetails.js
@@ -0,0 +1,273 @@
+var json_2023A = { 
+"name" : "森女风小黑裙" , 
+"price" : "¥ 53" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/691/900/15631009196_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/105/600/15631006501_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/442/600/15631006244_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/694/309/15692903496_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/869/678/15692876968_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/804/410/15693014408_1537626798.jpg"
+]
+};
+var json_5070 = { 
+"name" : "蕾丝彩虹裙" , 
+"price" : "¥ 49" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/018/361/15676163810_1537626798.jpg" ,
+	"./images/c01.jpg" ,
+	"./images/c02.jpg" ,
+	"./images/c03.jpg" ,
+	"./images/c04.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/378/765/15678567873_1537626798.jpg"
+]
+};
+var json_C6585 = { 
+"name" : "领格子泡泡袖裙子" , 
+"price" : "¥ 38" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/804/525/15624525408_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/139/696/15734696931_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/363/315/15624513363_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/589/894/15624498985_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/320/825/15624528023_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/451/837/15734738154_1537626798.jpg" 
+]
+};
+var json_5069 = { 
+"name" : "民国风格子连衣裙" , 
+"price" : "¥ 45" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/543/923/15693329345_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/285/113/15693311582_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/835/869/15583968538_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/261/053/15693350162_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/549/024/15631420945_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/993/714/15631417399_1537626798.jpg" 
+]
+};
+var json_5057 = { 
+"name" : "海军领格子裙儿童学院风背心裙" , 
+"price" : "¥ 40" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/420/868/14730868024_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/994/217/14690712499_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/986/167/14784761689_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/347/257/14784752743_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/725/727/14690727527_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/332/328/14690823233_1537626798.jpg" 
+]
+};
+var json_5086 = { 
+"name" : "复古拼接小碎花连衣裙" , 
+"price" : "¥ 43" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/731/689/15693986137_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/052/779/15693977250_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/686/359/15693953686_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/904/479/15693974409_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/429/646/15584646924_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/495/341/15632143594_1537626798.jpg" 
+]
+};
+var json_5080A = { 
+"name" : "木耳边の吊带连衣裙" , 
+"price" : "¥ 38" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/137/332/15671233731_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/386/646/15623646683_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/005/572/15671275500_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/649/251/15671152946_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/759/949/15733949957_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/799/851/15671158997_1537626798.jpg" 
+]
+};
+var json_7069 = { 
+"name" : "宫廷风泡泡袖连衣裙" , 
+"price" : "¥ 39" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/488/102/15734201884_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/625/381/15734183526_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/186/365/15671563681_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/694/665/15671566496_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/568/561/15734165865_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/386/210/15624012683_1537626798.jpg"
+]
+};
+var json_B6335 = { 
+"name" : "满天星公主亮片裙" , 
+"price" : "¥ 58" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/237/878/14866878732_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2019/764/972/10862279467_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2019/339/258/10834852933_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2019/855/672/10862276558_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2019/858/989/10804989858_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2019/328/468/10834864823_1537626798.jpg" 
+]
+};
+var json_q5190703 = { 
+"name" : "韩版INS套装连衣裙" , 
+"price" : "格子裙¥ 37 背带裙¥ 39" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/174/070/14837070471_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/666/540/14795045666_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/297/528/14740825792_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/014/135/14691531410_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/342/735/14691537243_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/428/884/14692488824_1537626798.jpg" 
+]
+};
+var json_5668 = { 
+"name" : "木耳边蓬蓬纱裙" , 
+"price" : "¥ 40" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/312/553/14907355213_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2019/766/996/10922699667_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2019/851/192/10894291158_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2019/107/996/10922699701_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2019/453/551/10864155354_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2019/722/252/10950252227_1537626798.jpg" 
+]
+};
+var json_2009 = { 
+"name" : "V领气质碎花裙" , 
+"price" : "¥ 46" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/889/897/14725798988_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/047/018/14685810740_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/806/688/14779886608_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/020/409/14779904020_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/007/897/14725798700_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/812/897/14725798218_1537626798.jpg"
+]
+};
+var json_2020 = { 
+"name" : "太阳花无袖连衣裙" , 
+"price" : "¥ 43" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/987/581/14726185789_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/876/671/14686176678_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/130/602/14686206031_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/367/492/14780294763_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/407/371/14726173704_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/691/215/14686512196_1537626798.jpg"
+]
+};
+var json_5058 = { 
+"name" : "樱花吊带连衣裙" , 
+"price" : "¥ 37" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/897/921/14731129798_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/389/460/14785064983_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/972/330/14691033279_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/595/880/14785088595_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/845/120/14691021548_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/837/070/14785070738_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/322/850/14785058223_1537626798.jpg"
+]
+};
+var json_C26047 = { 
+"name" : "油墨画小花连衣裙" , 
+"price" : "¥ 36" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/205/545/14787545502_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/723/022/14693220327_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/616/635/14787536616_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/560/065/14787560065_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/972/412/14693214279_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/246/271/14693172642_1537626798.jpg"
+]
+};
+var json_5060 = { 
+"name" : "彩色菊花连衣裙" , 
+"price" : "¥ 41" ,
+"addresses" : [
+	"https://cbu01.alicdn.com/img/ibank/2020/824/903/14691309428_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/906/373/14785373609_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/988/492/14691294889_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/938/264/14731462839_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/448/354/14731453844_1537626798.jpg" ,
+	"https://cbu01.alicdn.com/img/ibank/2020/678/553/14785355876_1537626798.jpg"
+]
+};
+window.onload = function() {
+	var temp =window.location.href;
+    var code=temp.split("?code=")[1];
+	var detailsJson = checkJson(code);
+	var detailsStr = '';
+	
+	if(null != detailsJson && "" != detailsJson){
+		detailsStr += '<div class="cont span_2_of_3" id="details">';
+		detailsStr += '<ul style="background: #7DB122;color:#FFFFFF;font-size: 32px;padding-left:10px;">';
+		detailsStr += detailsJson.name+' '+detailsJson.price+'</ul>';
+		detailsStr += '</br>';
+		detailsStr += '<div class="top-box">';
+		for (var i = 0; i < detailsJson.addresses.length; i++) {
+			detailsStr += '<div>';
+			detailsStr += '<img src="'+detailsJson.addresses[i]+'" alt=""/>';
+			detailsStr += '<div>';
+		}
+		detailsStr += '<div class="clear"></div>';
+		detailsStr += '</div>';
+	}
+	$("#details").html(detailsStr);
+}
+
+function checkJson(code){
+	var json = "";
+	switch(code) {
+     case "2023A":
+        json = json_2023A;
+        break;
+     case "5070":
+        json = json_5070;
+        break;
+     case "C6585":
+        json = json_C6585;
+        break;
+     case "5069":
+        json = json_5069;
+        break;
+     case "5057":
+        json = json_5057;
+        break;
+     case "5086":
+        json = json_5086;
+        break;
+     case "5080A":
+        json = json_5080A;
+        break;
+     case "7069":
+        json = json_7069;
+        break;
+     case "B6335":
+        json = json_B6335;
+        break;
+     case "q5190703":
+        json = json_q5190703;
+        break;
+     case "5668":
+        json = json_5668;
+        break;
+     case "2009":
+        json = json_2009;
+        break;
+     case "2020":
+        json = json_2020;
+        break;
+     case "5058":
+        json = json_5058;
+        break;
+     case "C26047":
+        json = json_C26047;
+        break;
+     case "5060":
+        json = json_5060;
+        break;
+     default:
+        json = "";
+}
+	
+	return json;
+}
